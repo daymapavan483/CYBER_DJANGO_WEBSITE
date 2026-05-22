3f944af0f80f8c98bb143ffd27bfabe006bd15b0
daymapavan483 <daymabirdichand483@gmail.com>
2026-05-22 21:40:31 +0530

    Update settings.py

--- security_system/settings.py
+++ security_system/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('django-insecure-my-secret-key-54321')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
